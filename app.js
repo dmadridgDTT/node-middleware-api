805fd8b2b6fc9e1e65bfa38c2c4af4404d6dc562
Daniel Madrid <daniel.madrid@disruptt.mx>
2022-08-15 18:41:55 -0600

    hotfix para SGC web producción y sandbox.

--- app.js
+++ app.js
@@ -50,46 +50,6 @@ app.get('/api/greeting', (request, response) => {
 // Password: ROOT
 // Database: sgc
 
-// app.post('/api/getServicios', (request, response) => {
-//   if (!request.body) return response.sendStatus(400);
-
-//   if (!request.body.credentials) {
-//     return response.status(401).json({ error: 'No credentials' });
-//   }
-
-//   const { host, user, password, db } = request.body.credentials;
-
-//   const credentials = {
-//     host: host,
-//     port: 3306,
-//     user: user,
-//     password: password,
-//     database: db,
-//     connectTimeout: 10000
-//   };
-
-//   try {
-//     const validateCredentials = databaseConnection(credentials);
-//     if (typeof (validateCredentials) === 'string') return response.status(401).json({ error: validateCredentials });
-
-//     const connection = mysql.createConnection(credentials);
-//     connection.connect(error => {
-//       if (error) return response.status(401).json({ error: `No connection in the db: ${error}` });
-//     });
-
-//     // connection.query('SELECT VERSION();', function (error, results, fields) {
-//     connection.query('SELECT serv.id_autotanque, serv.consecutivo2, serv.ts1, serv.volumen, cliente.cuenta, cliente.id_precio, serv.precio_str FROM ri505_servicio serv INNER JOIN cliente ON serv.id_Cliente = cliente.id_Cliente', function (error, results, fields) {
-//       if (error) {
-//         return response.status(401).json({ error: error });
-//       } else {
-//         return response.json({ data: results });
-//       }
-//     });
-//   } catch (error) {
-//     return response.status(401).send({ response: error });
-//   }
-// });
-
 app.post('/api/probarConexion', (request, response) => {
   const { host, user, password, db } = request.body;
   const credentials = {
@@ -367,7 +327,7 @@ app.post('/api/syncServicios', async (request, response) => {
   }
 });
 
-// SGC Web Ventas
+// SGC Web Ventas Sandbox
 
 // Get token
 const getToken = async () => {
@@ -405,7 +365,7 @@ const getToken = async () => {
   }
 };
 
-app.post('/api/web/procesarPeticion', jsonParser, async (request, resp) => {
+app.post('/api/web/sb_procesarPeticion', jsonParser, async (request, resp) => {
   const { modulo, accion, paquete } = request.body;
   const headers = {
     'Content-Type': 'text/xml; charset=utf-8',
@@ -414,6 +374,84 @@ app.post('/api/web/procesarPeticion', jsonParser, async (request, resp) => {
 
   const url = 'http://testpotogas.sgcweb.com.mx//ws/1094AEV2/v2/soap.php';
   const token = await getToken();
+
+  if (token === '') return resp.status(401).json({ error: 'No token' });
+
+  const xml = `<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sgc="http://www.sgcweb.com.mx/sgcweb">
+  <soapenv:Header/>
+  <soapenv:Body>
+     <sgc:procesarPeticion soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <session xsi:type="xsd:string">${token}</session>
+        <modulo xsi:type="xsd:string">${modulo}</modulo>
+        <accion xsi:type="xsd:string">${accion}</accion>
+        <paquete xsi:type="xsd:string">${JSON.stringify(paquete)}</paquete>
+     </sgc:procesarPeticion>
+  </soapenv:Body>
+</soapenv:Envelope>`;
+
+  try {
+    const { response } = await soapRequest({ url: url, headers: headers, xml: xml, timeout: 10000 });
+    const { body } = response;
+    const parser = new DOMParser();
+    const responseXML = parser.parseFromString(body, 'text/xml');
+    const codigo = responseXML.getElementsByTagName('codigo')[0].textContent;
+    if (codigo === '0000' || codigo === '1111') {
+      const informacion = JSON.parse(responseXML.getElementsByTagName('informacion')[0].textContent);
+      return resp.status(200).json({ codigo, informacion });
+    } else {
+      return resp.status(200).json({ error: 'Error', codigo: codigo });
+    }
+  } catch (e) {
+    console.log(e);
+    return resp.status(401).json({ error: 'Error procesando peticion', message: e });
+  }
+});
+
+// Get production token
+const getProductionToken = async () => {
+  // Soap request for token generation
+  const url = 'http://potogas.sgcweb.com.mx/ws/1094AEV2/v2/soap.php';
+  const headersTest = {
+    'Content-Type': 'text/xml; charset=utf-8',
+    SOAPAction: 'http://potogas.sgcweb.com.mx/ws/1094AEV2/v2/soap.php/login'
+  };
+  const username = 'apiuser';
+  const password = 'b53de80ae066f5ccd74892c2417a0f6d';
+  const xml = `<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sgc="http://www.sgcweb.com.mx/sgcweb" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/">
+  <soapenv:Header/>
+  <soapenv:Body>
+     <sgc:login soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <user_auth xsi:type="sgc:user_auth">
+           <user_name xsi:type="xsd:string">${username}</user_name>
+           <password xsi:type="xsd:string">${password}</password>
+        </user_auth>
+        <application_name xsi:type="xsd:string">?</application_name>
+        <name_value_list xsi:type="sgc:name_value_list" SOAP-ENC:arrayType="sgc:name_value[]"/>
+     </sgc:login>
+  </soapenv:Body>
+</soapenv:Envelope>`;
+
+  try {
+    const { response } = await soapRequest({ url: url, headers: headersTest, xml: xml, timeout: 10000 }); // Optional timeout parameter(milliseconds)
+    const { body } = response;
+    const parser = new DOMParser();
+    const responseXML = parser.parseFromString(body, 'text/xml');
+    const token = responseXML.getElementsByTagName('id')[0].textContent;
+    return token;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+app.post('/api/web/procesarPeticion', jsonParser, async (request, resp) => {
+  const { modulo, accion, paquete } = request.body;
+  const headers = {
+    'Content-Type': 'text/xml; charset=utf-8',
+    SOAPAction: 'http://potogas.sgcweb.com.mx/ws/1094AEV2/v2/soap.php/procesarPeticion'
+  };
+
+  const url = 'http://potogas.sgcweb.com.mx/ws/1094AEV2/v2/soap.php?wsdl';
+  const token = await getProductionToken();
 
   if (token === '') return resp.status(401).json({ error: 'No token' });
 
